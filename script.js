85445d451ba32c8a75ad10e7def0bc211a429956
Perkin <perkin.oleksiy@gmail.com>
2019-12-04 11:16:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+// one-line comment
+/*
+multi-line comment
+*/
+/*alert(1);
+[0,3].push()*/
+
+// var LeftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14; 
+
+// console.log(4/0);   
+// console.log ('mdauzh'*9);
+// let something;
+// console.log(something);
+// let persone = {
+//     name: "John",
+//     age: 25, 
+//     isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[0]);
+
+// // alert("Hello,world");
+
+// /* let answer = confirm("Are you here?");
+
+// console.log(answer); */
+
+// let answer = +prompt("Есть ли вам 18 лет?", "Да");
+
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isCheked = true,
+    isClosed = false;
+
+    console.log(isCheked && !isClosed);
+    console.log(isCheked || isClosed);
+    
+
+    
+    
